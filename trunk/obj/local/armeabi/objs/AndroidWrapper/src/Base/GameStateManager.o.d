fb79daf1dba6d6bfc340f18a5a7f2887593f549d
Ncjlee78@gmail.com <Ncjlee78@gmail.com@176b2a17-02d8-d4be-a579-273f25f5680e>
2011-05-11 08:08:54 +0000

    Code clean up

--- trunk/obj/local/armeabi/objs/AndroidWrapper/src/Base/GameStateManager.o.d
+++
@@ -1,9 +0,0 @@
-/cygdrive/c/Users/NcJlee/Desktop/Internship/Project/Cal3DonAndroid/obj/local/armeabi/objs/AndroidWrapper/src/Base/GameStateManager.o: \
- /cygdrive/c/Users/NcJlee/Desktop/Internship/Project/Cal3DonAndroid/jni/src/Base/GameStateManager.cpp \
- inc/Base/GameStateManager.h inc/GameState/GameState.h inc/Utils/Stack.h
-
-inc/Base/GameStateManager.h:
-
-inc/GameState/GameState.h:
-
-inc/Utils/Stack.h:
